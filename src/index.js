5ca0168e00b5d18e29c91f305c2889b13ce8105b
jaskiratsingh562 <jaskiratsingh562@gmail.com>
2022-09-14 23:10:29 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import SortVisual from './sort';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SortVisual/>
+  </React.StrictMode>
+);
